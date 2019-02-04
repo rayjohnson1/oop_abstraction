6d8ebbb82da0090ebb3406374abcc2412897de31
rayjohnson121 <35235312+rayjohnson121@users.noreply.github.com>
2019-02-03 22:04:35 -0500

    Finished Project

--- build/index.js
+++ build/index.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var _bank = _interopRequireDefault(require("./bank"));
+var _BankMember = _interopRequireDefault(require("./BankMember"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //index
-var chuck = new _bank.default(3000);
-console.log("\nDepositing Funds");
-chuck.deposit(150);
-console.log("Chuck's Savings Balance: $".concat(chuck.savingsBalance, " \n"));
-console.log("Using our set method to change Chuck's savingsBalance.... \n");
-chuck.savingsBalance = 0;
-console.log("Withdrawing Funds");
-chuck.withdraw(1650);
-console.log("Chuck's Savings Balance: $".concat(chuck.savingsBalance, " \n"));
+var chuck = new _BankMember.default("Chuck");
+console.log("\nCreating ".concat(chuck.name, "'s new savings account."));
+chuck.createSavingsAccount(3000);
+console.log("\nDepositing funds");
+chuck.depositToSavings(150);
+console.log("Chuck's Savings Balance: $".concat(chuck.getSavingsBalance(), " \n"));
+console.log("Withdrawing funds");
+chuck.withdrawFromSavings(1650);
+console.log("Chuck's Savings Balance: $".concat(chuck.getSavingsBalance(), " \n"));
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 //index
-import BankMember from './bank';
+import BankMember from './BankMember';
 
-const chuck = new BankMember(3000);
+const chuck = new BankMember(`Chuck`);
 
-console.log("\nDepositing Funds");
-chuck.deposit(150);
-console.log(`Chuck's Savings Balance: $${chuck.savingsBalance} \n`);
+console.log(`\nCreating ${chuck.name}'s new savings account.`);
+chuck.createSavingsAccount(3000);
 
-console.log("Using our set method to change Chuck's savingsBalance.... \n");
-chuck.savingsBalance = 0;
+console.log("\nDepositing funds");
+chuck.depositToSavings(150);
+console.log(`Chuck's Savings Balance: $${chuck.getSavingsBalance()} \n`);
 
-console.log("Withdrawing Funds");
-chuck.withdraw(1650);
-console.log(`Chuck's Savings Balance: $${chuck.savingsBalance} \n`);
+console.log("Withdrawing funds");
+chuck.withdrawFromSavings(1650);
+console.log(`Chuck's Savings Balance: $${chuck.getSavingsBalance()} \n`);
